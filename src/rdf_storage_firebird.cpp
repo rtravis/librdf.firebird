cf5be17c746fc4834731b89549d89a9ebaf0e403
Robert Zavalczki <toby_77@users.sourceforge.net>
2015-11-01 22:17:56 +0200

    plan queries involving blank nodes to use more selective indexes

--- src/rdf_storage_firebird.cpp
+++ src/rdf_storage_firebird.cpp
@@ -191,7 +191,8 @@ struct PreparedStatements
 		},
 		{
 			// SELECT_TRIPLE_7
-			"SELECT r.ID FROM TRIPLE r WHERE r.S_BLANK=? AND r.P_URI=? AND r.O_URI=? AND r.C_URI IS NULL",
+			"SELECT r.ID FROM TRIPLE r WHERE r.S_BLANK=? AND r.P_URI=? AND r.O_URI=? AND r.C_URI IS NULL"
+            " PLAN (r INDEX (IDX_TRIPLE_S_BLANK, IDX_TRIPLE_O_URI))",
 			nullptr,
 			3, 1
 		},
@@ -203,7 +204,8 @@ struct PreparedStatements
 		},
 		{
 			// SELECT_TRIPLE_9
-			"SELECT r.ID FROM TRIPLE r WHERE r.S_BLANK=? AND r.P_URI=? AND r.O_BLANK=? AND r.C_URI IS NULL",
+			"SELECT r.ID FROM TRIPLE r WHERE r.S_BLANK=? AND r.P_URI=? AND r.O_BLANK=? AND r.C_URI IS NULL"
+		    " PLAN (r INDEX (IDX_TRIPLE_S_BLANK, IDX_TRIPLE_O_BLANK))",
 			nullptr,
 			3, 1
 		},
